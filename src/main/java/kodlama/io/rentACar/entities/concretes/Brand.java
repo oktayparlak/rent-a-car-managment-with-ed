ade25e9c208e28226bc7cd6606ca0c3388d873cc
Oktay⚡ <oktay.parlak.12@hotmail.com>
2022-11-05 21:59:36 +0300

    the server runned

--- src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
+++ src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -1,8 +1,23 @@
 package kodlama.io.rentACar.entities.concretes;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "brands")
 public class Brand {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "name")
     private String name;
 
 }
