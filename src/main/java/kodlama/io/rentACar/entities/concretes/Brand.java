5d30b234c9c314787f08930501c5c4525cb2aa5c
Oktay⚡ <oktay.parlak.12@hotmail.com>
2022-11-07 15:36:00 +0300

    day 5 completed

--- src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
+++ src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -7,16 +7,17 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "brands")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Brand {
 
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(name = "name")
     private String name;
 
