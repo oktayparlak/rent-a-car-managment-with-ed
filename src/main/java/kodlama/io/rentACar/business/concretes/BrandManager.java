d3595cb630caf70c8be42737e8eb3dd17358cd36
Oktay <oktay.parlak.12@hotmail.com>
2023-02-26 18:20:33 +0300

    day 6 completed

--- src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
+++ src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -2,7 +2,10 @@ package kodlama.io.rentACar.business.concretes;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.requests.UpdateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.responses.GetByIdBrandResponse;
+import kodlama.io.rentACar.core.utilities.mappers.ModelMapperService;
 import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
 import kodlama.io.rentACar.entities.concretes.Brand;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,39 +13,67 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.stream.Collectors;
 
 @Service
 public class BrandManager implements BrandService {
 
     private BrandRepository brandRepository;
+    private ModelMapperService modelMapperService;
 
     @Autowired
-    public BrandManager(BrandRepository brandRepository) {
+    public BrandManager(BrandRepository brandRepository, ModelMapperService modelMapperService) {
         this.brandRepository = brandRepository;
+        this.modelMapperService = modelMapperService;
     }
 
     @Override
     public List<GetAllBrandsResponse> getAll() {
 
+//        List<Brand> brands = brandRepository.findAll();
+//        List<GetAllBrandsResponse> brandsResponses = new ArrayList<GetAllBrandsResponse>();
+//
+//        for (Brand brand : brands) {
+//            GetAllBrandsResponse responseItem = new GetAllBrandsResponse();
+//            responseItem.setId(brand.getId());
+//            responseItem.setName(brand.getName());
+//
+//            brandsResponses.add(responseItem);
+//        }
+
         List<Brand> brands = brandRepository.findAll();
-        List<GetAllBrandsResponse> brandsResponses = new ArrayList<GetAllBrandsResponse>();
+        List<GetAllBrandsResponse> brandsResponses = brands.stream().map(brand ->
+            this.modelMapperService.forResponse().map(brand, GetAllBrandsResponse.class)).collect(Collectors.toList());
 
-        for (Brand brand: brands) {
-            GetAllBrandsResponse responseItem = new GetAllBrandsResponse();
-            responseItem.setId(brand.getId());
-            responseItem.setName(brand.getName());
-
-            brandsResponses.add(responseItem);
-        }
         return brandsResponses;
     }
 
     @Override
     public void add(CreateBrandRequest createBrandRequest) {
-        Brand brand = new Brand();
-        brand.setName(createBrandRequest.getName());
+
+        //Brand brand = new Brand();
+        //brand.setName(createBrandRequest.getName());
+
+        Brand brand = this.modelMapperService.forRequest().map(createBrandRequest, Brand.class);
 
         this.brandRepository.save(brand);
+    }
+
+    @Override
+    public GetByIdBrandResponse getById(int id) {
+        Brand brand = this.brandRepository.findById(id).orElseThrow();
+        GetByIdBrandResponse response = this.modelMapperService.forResponse().map(brand, GetByIdBrandResponse.class);
+        return response;
+    }
+
+    @Override
+    public void update(UpdateBrandRequest updateBrandRequest) {
+        Brand brand = this.modelMapperService.forRequest().map(updateBrandRequest, Brand.class);
+        this.brandRepository.save(brand);
+    }
+
+    @Override
+    public void delete(int id) {
+        this.brandRepository.deleteById(id);
     }
 }
--- src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
+++ src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -21,4 +21,5 @@ public class Brand {
     @Column(name = "name")
     private String name;
 
+
 }
